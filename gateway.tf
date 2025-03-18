f4a91120c452fe7d11c784481a64436e07ee4792
Gregori Simei <40555037+GregoriSimei@users.noreply.github.com>
2025-03-18 10:04:28 -0300

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -4,7 +4,7 @@ resource "aws_apigatewayv2_api" "ecs_api" {
 
   cors_configuration {
     allow_origins = ["*"]
-    allow_methods = ["POST", "GET", "OPTIONS"]
+    allow_methods = ["POST", "GET", "OPTIONS", "DELETE", "PATCH", "PUT"]
     allow_headers = ["content-type", "authorization"]
     max_age = 300
   }
@@ -13,21 +13,19 @@ resource "aws_apigatewayv2_api" "ecs_api" {
 resource "aws_apigatewayv2_integration" "ecs_integration" {
   api_id             = aws_apigatewayv2_api.ecs_api.id
   integration_type   = "HTTP_PROXY"
-  integration_uri    = "http://${aws_eip.api_eip.public_ip}:80"
+  integration_uri    = "http://${aws_eip.api_eip.public_ip}/{proxy}"
   integration_method = "ANY"
-  connection_type    = "INTERNET"
-  payload_format_version = "1.0"
 }
 
 resource "aws_apigatewayv2_route" "ecs_route" {
   api_id    = aws_apigatewayv2_api.ecs_api.id
-  route_key = "ANY /client"
+  route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.ecs_integration.id}"
 }
 
 resource "aws_apigatewayv2_stage" "ecs_stage" {
   api_id      = aws_apigatewayv2_api.ecs_api.id
-  name        = "tech-challenge"
+  name        = "$default"
   auto_deploy = true
 
   access_log_settings {
