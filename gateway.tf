da37d266007c289cf98130898c3fdbb435ac5da7
GregoriSimei <gregorisimei@gmail.com>
2025-03-18 01:33:36 -0300

    test

--- gateway.tf
+++ gateway.tf
@@ -7,6 +7,7 @@ resource "aws_apigatewayv2_integration" "ecs_integration" {
   api_id             = aws_apigatewayv2_api.ecs_api.id
   integration_type   = "HTTP_PROXY"
   integration_uri    = "http://${aws_eip.api_eip.public_ip}"
+  integration_method = "ANY"
   connection_type    = "INTERNET"
   payload_format_version = "1.0"
 }
