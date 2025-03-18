9f6d3c512f86aebde98b66bf00506776efb441ab
GregoriSimei <gregorisimei@gmail.com>
2025-03-18 01:41:51 -0300

    test

--- gateway.tf
+++ gateway.tf
@@ -23,3 +23,42 @@ resource "aws_apigatewayv2_stage" "ecs_stage" {
   name        = "tech-challenge"
   auto_deploy = true
 }
+
+resource "aws_apigatewayv2_route_response" "ecs_route_response" {
+  api_id       = aws_apigatewayv2_api.ecs_api.id
+  route_id     = aws_apigatewayv2_route.ecs_route.id
+  route_response_key = "$default"
+  
+  response_templates = {
+    "application/json" = ""
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "ecs_api_log_group" {
+  name = "/aws/apigateway/ecs-api-access-logs"
+}
+
+resource "aws_apigatewayv2_stage" "ecs_stage" {
+  api_id      = aws_apigatewayv2_api.ecs_api.id
+  name        = "tech-challenge"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.ecs_api_log_group.arn
+    format          = jsonencode({
+      requestTime       = "$context.requestTime"
+      httpMethod       = "$context.httpMethod"
+      status           = "$context.status"
+      routeKey         = "$context.routeKey"
+      requestId        = "$context.requestId"
+      identitySource   = "$context.identity.sourceIp"
+      identityUserAgent = "$context.identity.userAgent"
+    })
+  }
+}
