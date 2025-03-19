bcc3f435a03ee297e5d0ca7dada9732645ae3b0a
GregoriSimei <gregorisimei@gmail.com>
2025-03-19 00:59:36 -0300

    ci: remove all terraform modification

---
+++ eip.tf
@@ -0,0 +1,12 @@
+resource "aws_eip" "api_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "api_eip"
+  }
+}
+
+resource "aws_eip_association" "api_eip_association" {
+  instance_id   = data.terraform_remote_state.network.outputs.api_instance_id
+  allocation_id = aws_eip.api_eip.id
+}
